8a4b52ce6b50dfc87c1cdf5ed1cb102e2513f3a8
waynetsai <wayne040904007@gmail.com>
2017-10-25 11:03:28 +0800

    Add files via upload

---
+++ 10551031.java
@@ -0,0 +1,8 @@
+class homework10551031{
+    public static void main(String[] args) {
+        
+        System.out.println("Örülök");
+        System.out.println("මම සතුටුයි");
+        System.out.println("나는 기분이 좋다.");
+    }
+}
